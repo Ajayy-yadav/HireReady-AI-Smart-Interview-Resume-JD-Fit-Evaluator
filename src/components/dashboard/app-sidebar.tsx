a1f3dfc04b3993ec17239f57d34a88dc42c26701
Vishnuuu18 <pyatlavishnuvardhan@gmail.com>
2025-07-25 20:37:56 +0530

    sidebarfooteer

--- src/components/dashboard/app-sidebar.tsx
+++ src/components/dashboard/app-sidebar.tsx
@@ -1,8 +1,11 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-
+import { RxDashboard } from "react-icons/rx";
+import { AiOutlineFileSearch } from "react-icons/ai";
+import { BsPersonVideo2 } from "react-icons/bs";
+import { RiFileHistoryLine } from "react-icons/ri";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -10,34 +13,31 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import Image from "next/image"
 
 // Menu items.
 const items = [
   {
-    title: "Home",
+    title: "Dashboard",
     url: "#",
-    icon: Home,
+    icon: RxDashboard,
   },
   {
-    title: "Inbox",
+    title: "JD Analyzer",
     url: "#",
-    icon: Inbox,
+    icon: AiOutlineFileSearch,
   },
   {
-    title: "Calendar",
+    title: "Mock Interview",
     url: "#",
-    icon: Calendar,
+    icon: BsPersonVideo2,
   },
   {
-    title: "Search",
+    title: "Activity Log",
     url: "#",
-    icon: Search,
+    icon: RiFileHistoryLine ,
   },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
+  
 ]
 
 export function AppSidebar() {
@@ -48,15 +48,25 @@ export function AppSidebar() {
     >
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
+          <SidebarGroupLabel className="flex gap-2 justify-center items-center">
+            <Image
+                  src="/assets/online-recruitment.png"
+                  height="25"
+                  width="25"
+                  alt="logo"
+            />
+            <span className="bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent font-bold text-lg">
+                  HireReady-AI
+                </span>
+          </SidebarGroupLabel>
+          <SidebarGroupContent className="pt-5">
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton asChild className="h-10">
                     <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
+                      <item.icon size={20}/>
+                      <span className="text-base">{item.title}</span>
                     </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
@@ -65,6 +75,28 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild className="h-10">
+              <div className="flex gap-3 w-full items-center">
+                <Image
+                  className="shrink-0"
+                  src="/assets/User.png"
+                  height="40"
+                  width="40"
+                  alt="User"
+                />
+                <span className="flex flex-col flex-1 min-w-0 truncate sidebar-expanded:inline sidebar-collapsed:hidden">
+                  <p className="font-semibold text-base truncate">Username</p>
+                  <p className="font-medium text-sm truncate">role</p>
+                </span>
+              </div>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        
+      </SidebarFooter>
     </Sidebar>
   )
 }
